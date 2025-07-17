8cf03e9727fca9675941ed290c38e65739443c2d
Rajat <rajatsharma1234599@gmail.com>
2025-07-17 19:26:43 +0530

    feat: did some stuff

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include "utils.h"
 
 using namespace std;
 
+void drawBox() {
+    constexpr int BorderHeight = 20;
+    constexpr int BorderWidth = 70;
+    int x = 0, y = 0;
+    x = BorderHeight / 2;
+    y = BorderWidth /2;
+    cout << x << " " << y << endl;
+    for (int i = 0; i < BorderHeight; i++) {
+        for (int j = 0; j < BorderWidth; j++) {
+            if (i==0 || i==BorderHeight - 1 || j == 0) {
+                cout<< "*";
+            }else if (i == x && j == y) {
+                cout << "@"; // Center
+            }else {
+                cout<< " ";
+            }
+        }
+        cout<<"*";
+        cout << endl;
+    }
+}
+
 int main() {
 
+    printMessage();
     const char *msg = "oooooooooo@";
     cout<<msg<<endl;
+
+    printInColor("Press Any Key to continue","white",true);
+    cout << endl;
+waitForKeyPress();
+
+    drawBox();
+
     int count = 0;
     while(true) {
         cout<<" ";
@@ -14,8 +45,6 @@ int main() {
         }
         count++;
     }
-
-
 
     return 0;
 }
---
+++ snake/utils.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rajat on 4/7/25.
+//
+#include <iostream>
+#include "utils.h"
+
+void printMessage() {
+    std::cout << "Hello from utils!" << std::endl;
+}
